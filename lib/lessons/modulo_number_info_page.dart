d23f1a9a3fcda575f8197f2227c2ad2c5849464b
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 18:13:38 -0700

    add modulo lesson page

---
+++ lib/lessons/modulo_number_info_page.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart'; // Import the FlipCard package
+
+class ModuloNumberInfoPage extends StatelessWidget {
+  final GlobalKey<FlipCardState> _flipCardKey = GlobalKey<FlipCardState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('What are Modulo Numbers?'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FlipCard(
+            key: _flipCardKey, // Assign the GlobalKey to the FlipCard
+            direction:
+                FlipDirection.HORIZONTAL, // Set the direction of the flip
+            flipOnTouch: true,
+            front: _buildFront(context),
+            back: _buildBack(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFront(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Modulo numbers, also known as remainders, are the result of a division operation.',
+              style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'When you divide one integer by another, the modulo operation returns the remainder after the division.',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBack(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Los números de módulo, también conocidos como residuos, son el resultado de una operación de división.',
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Cuando divides un número entero por otro, la operación de módulo devuelve el residuo después de la división.',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExamples(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20),
+        Text(
+          'Here are a few examples of modulo numbers:',
+          style: TextStyle(
+              fontSize: 24,
+              color: Colors.white), // Increased font size and colored text
+        ),
+        SizedBox(height: 20),
+        _buildBulletPoint(context,
+            '10 % 3 = 1: When you divide 10 by 3, the remainder is 1.'),
+        _buildBulletPoint(context,
+            '15 % 4 = 3: When you divide 15 by 4, the remainder is 3.'),
+        _buildBulletPoint(context,
+            '21 % 5 = 1: When you divide 21 by 5, the remainder is 1.'),
+        SizedBox(height: 20),
+      ],
+    );
+  }
+
+  Widget _buildBulletPoint(BuildContext context, String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.circle,
+              size: 20,
+              color: Colors.white, // Change bullet point color to white
+            ),
+          ),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
