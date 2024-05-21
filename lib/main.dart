1ea777267de49ace58211749ce0eb0685acf54d4
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 17:23:19 -0700

    fix main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,73 @@
 import 'package:flutter/material.dart';
+import 'lessons_page.dart';
+//import 'game_list_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Numquest',
       home: Scaffold(
         body: Stack(
-          fit: StackFit.expand,
           children: <Widget>[
-            // Background Image
-            Image.asset(
-              '/Users/manyasohal/num_quest/assets/vecteezy_meadow-panorama-3d-illustration-bright-landscape-of-green_20570946.jpg',
-              fit: BoxFit.cover,
+            Positioned.fill(
+              child: Image.asset(
+                '/Users/manyasohal/num_quest/assets/vecteezy_meadow-panorama-3d-illustration-bright-landscape-of-green_20570946.jpg',
+                fit: BoxFit.cover,
+              ),
             ),
-            // Overlay
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // Title
-                Text(
-                  'NUMQUEST',
-                  style: TextStyle(
-                    fontSize: 90,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Raleway',
-                    color: Colors.orange,
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'NUMQUEST',
+                    style: TextStyle(
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'RaleWay',
+                      color: Colors.orange,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                // Buttons Row
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    RoundedButton(
-                      text: 'LEARN',
-                      onPressed: () {
-                        // Navigate to learn page or perform desired action
-                      },
-                    ),
-                    SizedBox(width: 20),
-                    RoundedButton(
-                      text: 'PLAY',
-                      onPressed: () {
-                        // Navigate to play page or perform desired action
-                      },
-                    ),
-                    SizedBox(width: 20),
-                    RoundedButton(
-                      text: 'SETTINGS',
-                      onPressed: () {
-                        // Navigate to settings page or perform desired action
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RoundedButton(
+                        text: 'LESSONS',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LessonsPage()),
+                          );
+                        },
+                      ),
+                      SizedBox(width: 40),
+                      RoundedButton(
+                        text: 'PLAY',
+                        onPressed: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(builder: (context) => GameListPage()),
+                          // );
+                        },
+                      ),
+                      SizedBox(width: 40),
+                      RoundedButton(
+                        text: 'SETTINGS',
+                        onPressed: () {
+                          // Navigate to settings page or perform desired action
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -72,29 +78,31 @@ class MyApp extends StatelessWidget {
 
 class RoundedButton extends StatelessWidget {
   final String text;
-  final Function()? onPressed;
+  final Function onPressed;
 
-  const RoundedButton({required this.text, this.onPressed, Key? key})
-      : super(key: key);
+  RoundedButton({required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: () => onPressed(),
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
+            borderRadius:
+                BorderRadius.circular(20.0), // Increased border radius
+            side: BorderSide(color: Colors.white),
           ),
         ),
         padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-          EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+          EdgeInsets.symmetric(
+              vertical: 20.0, horizontal: 30.0), // Increased padding
         ),
       ),
       child: Text(
         text,
-        style: TextStyle(fontSize: 24),
+        style: TextStyle(fontSize: 24), // Increased font size
       ),
     );
   }
