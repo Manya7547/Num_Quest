3b2bd9e6157ba1d02fb4e739d592a24feb868e23
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 18:08:37 -0700

    add factor lesson page

---
+++ lib/lessons/factors_info_page.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+
+class FactorsInfoPage extends StatelessWidget {
+  final GlobalKey<FlipCardState> _flipCardKey = GlobalKey<FlipCardState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('What are Factors?'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FlipCard(
+            key: _flipCardKey, // Assign the GlobalKey to the FlipCard
+            direction:
+                FlipDirection.HORIZONTAL, // Set the direction of the flip
+            flipOnTouch: true,
+            front: _buildFront(context),
+            back: _buildBack(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFront(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Factors are numbers that can be multiplied together to get another number.',
+              style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Imagine you have some objects and you can arrange them into rows and columns to form a rectangle, then the numbers of rows and columns you can create are factors of the total number of objects!',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBack(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Los factores son números que pueden multiplicarse entre sí para obtener otro número.',
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Imagina que tienes algunas cosas y puedes organizarlas en filas y columnas para formar un rectángulo, entonces los números de filas y columnas que puedes crear son factores del número total de objetos.',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExamples(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20),
+        Text(
+          'Here are a few examples of factors:',
+          style: TextStyle(
+              fontSize: 24,
+              color: Colors.white), // Increased font size and colored text
+        ),
+        SizedBox(height: 20),
+        _buildBulletPoint(context, 'Factors of 12: 1, 2, 3, 4, 6, 12'),
+        _buildBulletPoint(context, 'Factors of 20: 1, 2, 4, 5, 10, 20'),
+        _buildBulletPoint(
+            context, 'Factors of 36: 1, 2, 3, 4, 6, 9, 12, 18, 36'),
+        SizedBox(height: 20),
+      ],
+    );
+  }
+
+  Widget _buildBulletPoint(BuildContext context, String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.circle,
+              size: 20,
+              color: Colors.white, // Change bullet point color to white
+            ),
+          ),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
