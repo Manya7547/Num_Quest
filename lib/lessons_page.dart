eec2dfa3c2765ad8775b65db24be8f6fbec595c1
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 17:23:35 -0700

    add lessons page

---
+++ lib/lessons_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+// import 'lessons/even_number_info_page.dart';
+// import 'lessons/odd_number_info_page.dart';
+// import 'lessons/prime_number_info_page.dart';
+// import 'lessons/composite_number_info_page.dart';
+// import 'lessons/triangular_number_info_page.dart';
+// import 'lessons/perfect_number_info_page.dart';
+// import 'lessons/square_number_info_page.dart';
+// import 'lessons/fibonacci_number_info_page.dart';
+// import 'lessons/factors_info_page.dart';
+// import 'lessons/cube_number_info_page.dart';
+// import 'lessons/modulo_number_info_page.dart';
+
+class LessonsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LESSON PLAN'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                '/Users/manyasohal/num_quest/assets/lesson_page.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(100, 50, 100, 50),
+            child: GridView.count(
+              crossAxisCount: 4,
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 20.0,
+              shrinkWrap: true,
+              children: [
+                LessonButton(
+                  title: 'Even Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => EvenNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Odd Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => OddNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Prime Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => PrimeNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Composite Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => CompositeNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Triangular Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => TriangularNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Perfect Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => PerfectNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Square Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => SquareNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Fibonacci Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => FibonacciNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Factors',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => FactorsInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Cube Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => CubeNumberInfoPage()));
+                  },
+                ),
+                LessonButton(
+                  title: 'Modulo Numbers',
+                  onPressed: () {
+                    //Navigator.push(context, MaterialPageRoute(builder: (context) => ModuloNumberInfoPage()));
+                  },
+                ),
+                // Add more LessonButton widgets for each lesson
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LessonButton extends StatelessWidget {
+  final String title;
+  final Function()? onPressed;
+
+  LessonButton({required this.title, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.cyan.shade100,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0, // Remove button shadow
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            textAlign: TextAlign.center, // Align text in the center
+            style: TextStyle(
+              fontSize: 20, // Change font size
+              fontWeight: FontWeight.bold, // Make text bold
+              fontFamily: 'Arial', // Change font family if needed
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
