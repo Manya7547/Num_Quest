3e075fb4904856cf99f24a2a595d09944a67d38a
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 18:08:52 -0700

    add square lesson page

---
+++ lib/lessons/square_number_info_page.dart
@@ -0,0 +1,184 @@
+// TODO Implement this library. /Users/manyasohal/num_quest/assets/classboard.jpg
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart'; // Import the FlipCard package
+
+class SquareNumberInfoPage extends StatelessWidget {
+  final GlobalKey<FlipCardState> _flipCardKey = GlobalKey<FlipCardState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('What are Square Numbers?'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FlipCard(
+            key: _flipCardKey, // Assign the GlobalKey to the FlipCard
+            direction:
+                FlipDirection.HORIZONTAL, // Set the direction of the flip
+            flipOnTouch: true,
+            front: _buildFront(context),
+            back: _buildBack(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFront(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Square numbers are numbers that result from multiplying an integer by itself.',
+              style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'For example, 4 is a square number because 4 * 4 = 16.',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBack(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              '/Users/manyasohal/num_quest/assets/classboard.jpg'), // Set your background image here
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        color: Colors.black.withOpacity(0.5), // Add opacity to background color
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Los números cuadrados son números que resultan de multiplicar un número entero por sí mismo.',
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Por ejemplo, 4 es un número cuadrado porque 4 * 4 = 16.',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+            SizedBox(height: 20),
+            _buildExamples(context),
+            SizedBox(height: 20),
+            RotatedBox(
+              quarterTurns: 1, // Rotate the icon button 90 degrees
+              child: IconButton(
+                icon: Icon(Icons.translate),
+                onPressed: () {
+                  // Add logic to flip the card using the GlobalKey
+                  _flipCardKey.currentState?.toggleCard();
+                },
+              ),
+            ),
+            Text(
+              'Tap to Translate',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExamples(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20),
+        Text(
+          'Here are a few examples of square numbers:',
+          style: TextStyle(
+              fontSize: 24,
+              color: Colors.white), // Increased font size and colored text
+        ),
+        SizedBox(height: 20),
+        _buildBulletPoint(context, '4: 4 * 4 = 16'),
+        _buildBulletPoint(context, '9: 3 * 3 = 9'),
+        _buildBulletPoint(context, '16: 4 * 4 = 16'),
+        SizedBox(height: 20),
+      ],
+    );
+  }
+
+  Widget _buildBulletPoint(BuildContext context, String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.circle,
+              size: 20,
+              color: Colors.white, // Change bullet point color to white
+            ),
+          ),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white), // Increased font size and colored text
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
