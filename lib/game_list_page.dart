d0d7b25d5b07d91249dfac17015a332e826ad06b
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-20 19:06:13 -0700

    add imports

---
+++ lib/game_list_page.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'games/even_odd_sort.dart'; // Import the game widgets
+import 'games/word_problem_game.dart';
+import 'games/prime_number_game.dart';
+
+class GameListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ARCADE'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                '/Users/manyasohal/num_quest/assets/Bigschooldesk_generated.jpg'), // Set the background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(100, 50, 100, 50),
+          child: GridView.count(
+            crossAxisCount: 3, // Number of columns in the grid
+            crossAxisSpacing: 40.0,
+            mainAxisSpacing: 40.0,
+            children: <Widget>[
+              GameButton(
+                title: 'Number Placement Game',
+                onPressed: () {
+                  // Add navigation logic for Number Placement Game
+                },
+              ),
+              GameButton(
+                title: 'Number Pattern Match',
+                onPressed: () {
+                  // Add navigation logic for Number Pattern Match Game
+                },
+              ),
+              GameButton(
+                title: 'Even and Odd Sort',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EvenOddSortPage()),
+                  );
+                },
+              ),
+              GameButton(
+                title: 'Triangle Tower Builder',
+                onPressed: () {
+                  // Add navigation logic for Triangle Tower Builder Game
+                },
+              ),
+              GameButton(
+                title: 'Solve Some Fun Word Problems',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WordProblemsGame()),
+                  );
+                  // Add navigation logic for Solve Some Fun Word Problems Game
+                },
+              ),
+              GameButton(
+                title: 'Perfect Number Challenge',
+                onPressed: () {
+                  // Add navigation logic for Perfect Number Challenge Game
+                },
+              ),
+              GameButton(
+                title: 'Prime Explorer',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PrimeNumberGame()),
+                  );
+                  // Add navigation logic for Prime Explorer Game
+                },
+              ),
+              GameButton(
+                title: 'Factor Trees',
+                onPressed: () {
+                  // Add navigation logic for Factor Trees Game
+                },
+              ),
+              GameButton(
+                title: 'Fibonacci Sequence Puzzle',
+                onPressed: () {
+                  // Add navigation logic for Fibonacci Sequence Puzzle Game
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GameButton extends StatelessWidget {
+  final String title;
+  final Function()? onPressed;
+
+  GameButton({required this.title, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          title,
+          textAlign: TextAlign.center, // Align text in the center
+          style: TextStyle(
+            fontSize: 30, // Change font size
+            fontWeight: FontWeight.bold, // Make text bold
+            fontFamily: 'Arial', // Change font family if needed
+          ),
+        ),
+      ),
+    );
+  }
+}
