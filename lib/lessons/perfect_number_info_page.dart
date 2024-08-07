f55e955c72f77291f5f203b22541c2a923022a3b
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-08-06 19:34:57 -0700

    perfect number content update

--- lib/lessons/perfect_number_info_page.dart
+++ lib/lessons/perfect_number_info_page.dart
@@ -1,184 +1,206 @@
 import 'package:flutter/material.dart';
-import 'package:flip_card/flip_card.dart'; // Import the FlipCard package
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:num_quest/widgets/MultipageContainer.dart';
 
 class PerfectNumberInfoPage extends StatelessWidget {
-  final GlobalKey<FlipCardState> _flipCardKey = GlobalKey<FlipCardState>();
+  final FlutterTts flutterTts = FlutterTts();
+
+  void speakText(String text) async {
+    await flutterTts.setLanguage("en-US"); // Set language to English
+    await flutterTts.setPitch(1.0); // Set pitch
+    await flutterTts.speak(text);
+  }
+
+  void speakTextSpanish(String text) async {
+    await flutterTts.setLanguage("es-ES"); // Set language to Spanish
+    await flutterTts.setPitch(1.0); // Set pitch
+    await flutterTts.speak(text);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('What are Perfect Numbers?'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: FlipCard(
-            key: _flipCardKey, // Assign the GlobalKey to the FlipCard
-            direction:
-                FlipDirection.HORIZONTAL, // Set the direction of the flip
-            flipOnTouch: true,
-            front: _buildFront(context),
-            back: _buildBack(context),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildFront(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-              'assets/classboard.jpg'), // Set your background image here
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Container(
-        color: Colors.black.withOpacity(0.5), // Add opacity to background color
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Perfect numbers are numbers that are equal to the sum of their proper divisors, excluding the number itself.',
-              style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white), // Increased font size and colored text
+    return MultipageContainer(
+      pages: [
+        // First Page Content
+        (bool isEnglish) => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  color: Colors.white.withOpacity(0.8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          isEnglish
+                              ? 'A perfect number is a positive integer equal to the total of its positive divisors, except the number itself in number theory. For example, 6 is a perfect number since 1 + 2 + 3 equals 6.'
+                              : 'Un número perfecto es un número entero positivo que es igual a la suma de sus divisores positivos, excepto el propio número, en la teoría de números. Por ejemplo, 6 es un número perfecto ya que 1 + 2 + 3 es igual a 6.',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 18, 3, 48),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10),
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            isEnglish
+                                ? speakText(
+                                    'A perfect number is a positive integer equal to the total of its positive divisors, except the number itself in number theory. For example, 6 is a perfect number since 1 + 2 + 3 equals 6.')
+                                : speakTextSpanish(
+                                    'Un número perfecto es un número entero positivo que es igual a la suma de sus divisores positivos, excepto el propio número, en la teoría de números. Por ejemplo, 6 es un número perfecto ya que 1 + 2 + 3 es igual a 6.');
+                          },
+                        ),
+                        SizedBox(height: 50),
+                        Text(
+                          isEnglish
+                              ? 'Some of the first perfect numbers are 6, 28, 496, and 8128. Perfect numbers are also known as “Complete Numbers” and “Proper Numbers“.'
+                              : 'Algunos de los primeros números perfectos son 6, 28, 496 y 8128. Los números perfectos también se conocen como "Números Completos" y "Números Propios".',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 18, 3, 48),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10),
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            isEnglish
+                                ? speakText(
+                                    'Some of the first perfect numbers are 6, 28, 496, and 8128. Perfect numbers are also known as “Complete Numbers” and “Proper Numbers“.')
+                                : speakTextSpanish(
+                                    'Algunos de los primeros números perfectos son 6, 28, 496 y 8128. Los números perfectos también se conocen como "Números Completos" y "Números Propios".');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 350,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Image.asset('assets/perfect1.png'),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Text(
-              'For example, the first perfect number is 6 because its divisors are 1, 2, and 3, and 1 + 2 + 3 = 6.',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white), // Increased font size and colored text
+        // Second Page Content
+        (bool isEnglish) => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  color: Colors.white.withOpacity(0.8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          isEnglish
+                              ? 'The smallest perfect number example is the number six. However, there are also several other small perfect numbers whose divisors can be figured out and summed together manually. Below is a short, perfect number list that includes a few smaller perfect numbers. Please keep in mind that this list is not exhaustive.'
+                              : 'El ejemplo más pequeño de número perfecto es el número seis. Sin embargo, también hay varios otros números perfectos pequeños cuyos divisores pueden ser calculados y sumados manualmente. A continuación, se presenta una lista corta de números perfectos que incluye algunos de los números perfectos más pequeños. Por favor, tenga en cuenta que esta lista no es exhaustiva.',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 18, 3, 48),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10),
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            isEnglish
+                                ? speakText(
+                                    'The smallest perfect number example is the number six. However, there are also several other small perfect numbers whose divisors can be figured out and summed together manually. Below is a short, perfect number list that includes a few smaller perfect numbers. Please keep in mind that this list is not exhaustive.')
+                                : speakTextSpanish(
+                                    'El ejemplo más pequeño de número perfecto es el número seis. Sin embargo, también hay varios otros números perfectos pequeños cuyos divisores pueden ser calculados y sumados manualmente. A continuación, se presenta una lista corta de números perfectos que incluye algunos de los números perfectos más pequeños. Por favor, tenga en cuenta que esta lista no es exhaustiva.');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Image.asset('assets/perfect2.png', height: 400),
+              ],
             ),
-            SizedBox(height: 20),
-            _buildExamples(context),
-            SizedBox(height: 20),
-            RotatedBox(
-              quarterTurns: 1, // Rotate the icon button 90 degrees
-              child: IconButton(
-                icon: Icon(Icons.translate),
-                onPressed: () {
-                  // Add logic to flip the card using the GlobalKey
-                  _flipCardKey.currentState?.toggleCard();
-                },
-              ),
+        // Fourth Page Content
+        (bool isEnglish) => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  color: Colors.white.withOpacity(0.8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          isEnglish
+                              ? 'Now that you know what perfect numbers are let\'s go ahead and solve some fun problems.'
+                              : 'Ahora que sabes qué son los números perfecto, vamos a resolver algunos problemas divertidos.',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 18, 3, 48),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            isEnglish
+                                ? speakText(
+                                    'Now that you know what perfect numbers are let\'s go ahead and solve some fun problems.')
+                                : speakTextSpanish(
+                                    'Ahora que sabes qué son los números perfecto, vamos a resolver algunos problemas divertidos.');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Image.asset('assets/practice.png', height: 500),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //       builder: (context) => WordProblemPractice()),
+                    // );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    backgroundColor: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    isEnglish ? 'Quiz' : 'examen',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Tap to Translate',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white), // Increased font size and colored text
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBack(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-              'assets/classboard.jpg'), // Set your background image here
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Container(
-        color: Colors.black.withOpacity(0.5), // Add opacity to background color
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Los números perfectos son números que son iguales a la suma de sus divisores propios, excluyendo el propio número.',
-              style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.white), // Increased font size and colored text
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Por ejemplo, el primer número perfecto es 6 porque sus divisores son 1, 2 y 3, y 1 + 2 + 3 = 6.',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white), // Increased font size and colored text
-            ),
-            SizedBox(height: 20),
-            _buildExamples(context),
-            SizedBox(height: 20),
-            RotatedBox(
-              quarterTurns: 1, // Rotate the icon button 90 degrees
-              child: IconButton(
-                icon: Icon(Icons.translate),
-                onPressed: () {
-                  // Add logic to flip the card using the GlobalKey
-                  _flipCardKey.currentState?.toggleCard();
-                },
-              ),
-            ),
-            Text(
-              'Tap to Translate',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white), // Increased font size and colored text
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildExamples(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 20),
-        Text(
-          'Here are a few examples of perfect numbers:',
-          style: TextStyle(
-              fontSize: 24,
-              color: Colors.white), // Increased font size and colored text
-        ),
-        SizedBox(height: 20),
-        _buildBulletPoint(context, '6: 1 + 2 + 3 = 6'),
-        _buildBulletPoint(context, '28: 1 + 2 + 4 + 7 + 14 = 28'),
-        _buildBulletPoint(
-            context, '496: 1 + 2 + 4 + 8 + 16 + 31 + 62 + 124 + 248 = 496'),
-        SizedBox(height: 20),
       ],
-    );
-  }
-
-  Widget _buildBulletPoint(BuildContext context, String text) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Icon(
-              Icons.circle,
-              size: 20,
-              color: Colors.white, // Change bullet point color to white
-            ),
-          ),
-          Expanded(
-            child: Text(
-              text,
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white), // Increased font size and colored text
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
